e634d814f22db6d1d0de3ae3b107da29f3c2700e
johngaynor <jgaynr@icloud.com>
2025-05-26 18:32:20 -0400

    changed image path

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,17 +11,17 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#020617",
     icons: [
       {
-        src: "192x192.png",
+        src: "/192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "512x512.png",
+        src: "/512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
       {
-        src: "1024x1024.png",
+        src: "/1024x1024.png",
         sizes: "1024x1024",
         type: "image/png",
       },
