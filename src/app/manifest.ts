9e15054ff20a88ce8e1801ae2bfe3780e5af6910
johngaynor <jgaynr@icloud.com>
2025-05-26 18:24:49 -0400

    added in manifest for pwa

---
+++ src/app/manifest.ts
@@ -0,0 +1,30 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "PhysiQ",
+    short_name: "PhysiQ",
+    description: "Bodybuilding Redefined.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#0f172a",
+    theme_color: "#1e293b",
+    icons: [
+      {
+        src: "192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "1024x1024.png",
+        sizes: "1024x1024",
+        type: "image/png",
+      },
+    ],
+  };
+}
