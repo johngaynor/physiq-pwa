fbeadf8e3982d923ffcdfe76d1e745dcc235e09c
johngaynor <jgaynr@icloud.com>
2025-05-26 17:16:22 -0400

    Vercel pls accept

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 
-const Main: React.FC<React.ReactNode> = () => {
+const Main = () => {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <h1 className="scroll-m-20 font-extrabold tracking-tight text-8xl text-center">
