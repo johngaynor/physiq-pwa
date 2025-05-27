5fa0685cb9e30463fbfa2739a260747d34cc7410
johngaynor <jgaynr@icloud.com>
2025-05-27 18:07:46 -0400

    added in temporary supplements table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,32 @@
 "use client";
 import React from "react";
 import { useUser } from "@clerk/nextjs";
-import { H4, Button, H1, H5 } from "@/components/ui";
+import { H4, Button, H1, H5, Checkbox } from "@/components/ui";
 import { CirclePlus } from "lucide-react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+type SupplementItem = {
+  id: number;
+  name: string;
+  dosage: string;
+  checked: boolean;
+};
+
+const SupplementData: SupplementItem[] = [
+  { id: 1, name: "Creatine Monohydrate", dosage: "5g", checked: false },
+  { id: 2, name: "Pre-Workout", dosage: "1 scoop", checked: true },
+  { id: 3, name: "Noxygen Pump Powder", dosage: "1 scoop", checked: false },
+  { id: 4, name: "Glutamine", dosage: "5g", checked: false },
+  { id: 5, name: "D-Aspartic Acid", dosage: "9000mg", checked: false },
+  // { id: 6, name: "Berberine", dosage: "1 Softgel", checked: false },
+];
 
 const Main = () => {
   const { user } = useUser();
@@ -49,8 +73,8 @@ const Main = () => {
     );
   } else
     return (
-      <div>
-        <H4>Today</H4>
+      <div className="py-4">
+        <H4 className="pb-4">Today</H4>
         <div className="grid grid-cols-2 grid-rows-2 gap-1 h-full w-full border-2 rounded-md">
           <DashboardButton
             header="Weight"
@@ -72,6 +96,28 @@ const Main = () => {
             subheader="Today's training"
             // data="56m"
           />
+        </div>
+        <div className="border-2 p-2 rounded-md mt-2">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead></TableHead>
+                <TableHead>Supplement</TableHead>
+                <TableHead>Dosage</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {SupplementData.map((item) => (
+                <TableRow key={item.id}>
+                  <TableCell className="font-medium">
+                    <Checkbox checked={item.checked} />
+                  </TableCell>
+                  <TableCell>{item.name}</TableCell>
+                  <TableCell>{item.dosage}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </div>
       </div>
     );
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,3 +2,4 @@ export { Button } from "./button";
 export { Input } from "./input";
 export { Label } from "./label";
 export { H1, H2, H3, H4, H5, H6 } from "./typography";
+export { Checkbox } from "./checkbox";
