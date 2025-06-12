bedd7ad400eb92a1c8c806145fc22b787a2cab7a
johngaynor <jgaynr@icloud.com>
2025-06-12 10:03:49 -0400

    trying import workaround

---
+++ src/app/Health/index.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+import { connect, ConnectedProps } from "react-redux";
+import { RootState } from "../store/reducer";
+
+function mapStateToProps(state: RootState) {
+  return {
+    apps: state.app.apps,
+  };
+}
+
+const connector = connect(mapStateToProps, {});
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+const HealthPage: React.FC<PropsFromRedux> = ({ apps }) => {
+  const healthApps = apps?.filter((app) => app.route.includes("/health"));
+  console.log({ healthApps });
+  return <div className="w-full">HEALTH APPS</div>;
+};
+
+export default connector(HealthPage);
--- src/app/Health/page.tsx
+++ src/app/Health/page.tsx
@@ -1,21 +1,7 @@
 "use client";
 import React from "react";
-import { connect, ConnectedProps } from "react-redux";
-import { RootState } from "../store/reducer";
+import HealthPage from "./index";
 
-function mapStateToProps(state: RootState) {
-  return {
-    apps: state.app.apps,
-  };
+export default function Page() {
+  return <HealthPage />;
 }
-
-const connector = connect(mapStateToProps, {});
-type PropsFromRedux = ConnectedProps<typeof connector>;
-
-const HealthPage: React.FC<PropsFromRedux> = ({ apps }) => {
-  const healthApps = apps?.filter((app) => app.route.includes("/health"));
-  console.log({ healthApps });
-  return <div className="w-full">HEALTH APPS</div>;
-};
-
-export default connector(HealthPage);
