b94dc567782046b744bde27dad295164b2b307fe
johngaynor <jgaynr@icloud.com>
2025-06-12 10:18:13 -0400

    trying out new pages template

--- src/app/Health/page.tsx
+++ src/app/Health/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 import React from "react";
 import HealthPage from "./index";
+import PageTemplate from "../components/Page";
 
 export default function Page() {
-  return <HealthPage />;
+  return (
+    <PageTemplate title="Health Apps">
+      <HealthPage />
+    </PageTemplate>
+  );
 }
---
+++ src/app/components/Page/index.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React from "react";
+import { H4 } from "@/components/ui";
+import { usePathname } from "next/navigation";
+
+type PageTemplateProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+export default function PageTemplate({ children, title }: PageTemplateProps) {
+  const pathname = usePathname();
+  const segments = pathname.split("/").filter(Boolean); // Breadcrumb parts
+
+  return (
+    <div className="space-y-4">
+      <div className="text-sm text-muted-foreground">
+        {segments.map((segment, idx) => (
+          <span key={idx}>
+            {idx > 0 && " / "}
+            <span className="capitalize">{segment}</span>
+          </span>
+        ))}
+      </div>
+      <H4 className="py-4">{title}</H4>
+      <div className="flex flex-col md:flex-row gap-4 w-full">{children}</div>
+    </div>
+  );
+}
