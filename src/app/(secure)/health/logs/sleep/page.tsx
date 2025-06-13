3cc082450b9f9a7d65b13c3d7e3ad9bf9a35b3e4
johngaynor <jgaynr@icloud.com>
2025-06-12 20:25:02 -0400

    Fixed title

--- src/app/(secure)/health/logs/sleep/page.tsx
+++ src/app/(secure)/health/logs/sleep/page.tsx
@@ -4,7 +4,7 @@ import PageTemplate from "@/app/components/Page/PageTemplate";
 
 export default function Page() {
   return (
-    <PageTemplate title="Supplement Log">
+    <PageTemplate title="Sleep Log">
       <p>This feature is still being built. Check back later!</p>
     </PageTemplate>
   );
