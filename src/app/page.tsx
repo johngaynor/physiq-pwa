8c8ad696d7a121ff4c7e559f40a1846bde9f0c86
johngaynor <jgaynr@icloud.com>
2025-06-13 16:07:32 -0400

    removed last spinner import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import { useUser } from "@clerk/nextjs";
 import Dashboard from "./components/Dashboard";
-import { Spinner } from "@/components/ui";
 
 const Main = () => {
   const { user, isLoaded } = useUser();
@@ -11,10 +10,7 @@ const Main = () => {
     return (
       <div className="flex h-screen flex-col items-center justify-center">
         {!isLoaded && (
-          <>
-            <div className="fixed inset-0 bg-slate-200 dark:bg-[#020513] z-40" />
-            <Spinner size="large" />
-          </>
+          <div className="fixed inset-0 bg-slate-200 dark:bg-[#020513] z-40" />
         )}
 
         <h1 className="scroll-m-20 font-extrabold tracking-tight text-6xl md:text-8xl text-center">
