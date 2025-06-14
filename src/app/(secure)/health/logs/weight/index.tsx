80179797301d5cede4f57dc393109b44215f1484
johngaynor <jgaynr@icloud.com>
2025-06-13 20:30:56 -0400

    indicator

--- src/app/(secure)/health/logs/weight/index.tsx
+++ src/app/(secure)/health/logs/weight/index.tsx
@@ -51,7 +51,7 @@ const WeightLog: React.FC<PropsFromRedux> = ({
               mode="single"
               selected={date}
               onSelect={setDate}
-              className="rounded-md border shadow-sm w-full h-full"
+              className="rounded-md border shadow-sm w-full h-full bg-red-100"
               captionLayout="dropdown"
               dataDates={dailyLogs?.map((log) => new Date(log.date))}
             />
