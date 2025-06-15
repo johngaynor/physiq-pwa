fe40708259ff5ba68ad137016b0ca8b0662a98f9
johngaynor <jgaynr@icloud.com>
2025-06-15 09:08:25 -0400

    Added in trend notification

--- src/app/(secure)/health/logs/components/ChartLineMultiple.tsx
+++ src/app/(secure)/health/logs/components/ChartLineMultiple.tsx
@@ -9,6 +9,7 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
+  CardFooter,
 } from "@/components/ui/card";
 import {
   ChartConfig,
@@ -17,13 +18,12 @@ import {
   ChartTooltipContent,
 } from "@/components/ui/chart";
 import { DailyLog } from "../../state/types";
+import { TrendingUp, TrendingDown } from "lucide-react";
+import { DateTime } from "luxon";
 
 export const description = "An interactive line chart";
 
 const chartConfig = {
-  views: {
-    label: "Weight",
-  },
   today: {
     label: "Today",
   },
@@ -82,13 +82,27 @@ export function ChartLineMultiple({ dailyLogs = [] }: ChartProps) {
     };
   }, [sortedLogs]);
 
+  const labels = {
+    today: "all time",
+    last7: "7 days",
+    last30: "30 days",
+  };
+
+  const startingValue = filteredData.find((d) => d.weight);
+  const endingValue = [...filteredData].reverse().find((d) => d.weight);
+  const diff =
+    startingValue?.weight && endingValue?.weight
+      ? parseFloat((endingValue.weight - startingValue.weight).toFixed(2))
+      : 0;
+
   return (
     <Card className="py-4 sm:py-0">
       <CardHeader className="flex flex-col items-stretch border-b !p-0 sm:flex-row">
         <div className="flex sm:hidden lg:flex flex-1 flex-col justify-center gap-1 px-6 pb-3 sm:pb-0">
           <CardTitle>Morning Weight (lbs)</CardTitle>
           <CardDescription>
-            Showing weight over the last 30 days
+            Showing weight {activeChart !== "today" ? "over the last" : ""}{" "}
+            {labels[activeChart]}
           </CardDescription>
         </div>
         <div className="flex lg:w-auto w-full">
@@ -112,10 +126,10 @@ export function ChartLineMultiple({ dailyLogs = [] }: ChartProps) {
           })}
         </div>
       </CardHeader>
-      <CardContent className="px-2 sm:p-6">
+      <CardContent className="ms:p-2">
         <ChartContainer
           config={chartConfig}
-          className="aspect-auto h-[225px] w-full"
+          className="aspect-auto h-[200px] w-full"
         >
           <LineChart
             accessibilityLayer
@@ -160,6 +174,29 @@ export function ChartLineMultiple({ dailyLogs = [] }: ChartProps) {
           </LineChart>
         </ChartContainer>
       </CardContent>
+      <CardFooter>
+        <div className="flex w-full items-start gap-2 text-sm -mt-4 py-5">
+          <div className="flex flex-col lg:flex-row items-start gap-2 w-full">
+            <div className="flex items-center gap-2 leading-none font-medium">
+              Trending {diff >= 0 ? "up" : "down"} by {Math.abs(diff)} lbs{" "}
+              {activeChart !== "today" ? "over the last" : ""}{" "}
+              {labels[activeChart]}
+              {diff >= 0 ? (
+                <TrendingUp className="h-4 w-4" />
+              ) : (
+                <TrendingDown className="h-4 w-4" />
+              )}
+            </div>
+            <div className="text-muted-foreground flex items-center gap-2 leading-none">
+              {DateTime.fromISO(filteredData[0]?.date).toFormat("LLL dd yyyy")}{" "}
+              -{" "}
+              {DateTime.fromISO(
+                filteredData[filteredData.length - 1]?.date
+              ).toFormat("LLL dd yyyy")}
+            </div>
+          </div>
+        </div>
+      </CardFooter>
     </Card>
   );
 }
