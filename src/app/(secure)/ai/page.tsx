38f1960694828cb6666510e50dac538e73f73ec7
johngaynor <jgaynr@icloud.com>
2025-07-15 18:37:12 -0400

    added default apps page for ai

---
+++ src/app/(secure)/ai/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import PageTemplate from "../components/Templates/PageTemplate";
+import AppsGrid from "@/app/components/Page/AppsGrid";
+
+const title = "AI";
+
+export default function Page() {
+  return (
+    <PageTemplate title={title}>
+      <AppsGrid filter="ai" />
+    </PageTemplate>
+  );
+}
+
+export const metadata = {
+  title: `${title} | Physiq`,
+};
