b24ae4c0417b2bb222af642fa2da22b1a738e146
johngaynor <jgaynr@icloud.com>
2025-07-23 13:13:11 -0400

    Added success message for creating account

--- src/app/components/SessionInitializer/index.ts
+++ src/app/components/SessionInitializer/index.ts
@@ -5,6 +5,7 @@ import { useAuth, useUser } from "@clerk/nextjs";
 import { setToken } from "@/lib/apiClient";
 import { useDispatch } from "react-redux";
 import { initializeUser } from "@/app/(secure)/state/actions";
+import { toast } from "sonner";
 
 export default function SessionInitializer() {
   const auth = useAuth();
@@ -16,13 +17,17 @@ export default function SessionInitializer() {
       const token = await auth.getToken();
       setToken(token);
       if (isLoaded && user) {
-        dispatch<any>(
+        // Call initializeUser and check for 'existed' property
+        const result = await dispatch<any>(
           initializeUser(
             user.id,
             user.primaryEmailAddress?.emailAddress || "",
             user.fullName || ""
           )
         );
+        if (result && typeof result === "object" && result.existed === false) {
+          toast.success("Thanks for creating an account!");
+        }
       }
     };
     initializeSession();
